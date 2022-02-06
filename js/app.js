1f6d6db5c071d98ba5d0fb5ba88a3034dde53c68
dcollis92 <davidphilipcollis@gmail.com>
2022-02-06 08:50:37 -0600

    add to init fn and timer functionality

--- js/app.js
+++ js/app.js
@@ -25,38 +25,51 @@ const winningCombos = [ // 4.1 Array of Arrays
 // console.log(winningCombos)
 
 /*---------------------------- Variables (state) ----------------------------*/
-let squares, turn, winner;
+let squares, turn, winner, seconds, tickInterval, waitingForTimeout;
     // 1.1 // 1.2 // 1.3
 
 /*------------------------ Cached Element References ------------------------*/
 const squaresArray = document.querySelectorAll('.square') // 2.1 Squares Array
 const gameStatus = document.querySelector('#message') // 2.2
-const resetButton = document.querySelector('#reset-button') // 6.2
+const resetDiv = document.getElementById('reset-div')
+const resetBtn = document.querySelector('#reset-button') // 6.2
 
 /*----------------------------- Event Listeners -----------------------------*/
-
-console.log(squaresArray)
-
 squaresArray.forEach(square => square.addEventListener('click', handleClick)) // 3.2.1
+
+resetBtn.addEventListener('click', function (evt) {
+  if (evt.target.classList.contains("square")) {
+    resetDiv.classList.remove("hidden")
+  }
+})
+// DOES NOT WORK
 
 /*-------------------------------- Functions --------------------------------*/
 init() // 3.1
 
 function init() {
-    boardArray = [
-      null, null, null, 
-      null, null, null, 
-      null, null, null] // 3.2.1
+    boardArray = 
+      [ null, null, null, 
+        null, null, null, 
+        null, null, null ] // 3.2.1
+    message = "let X starts the game"
     turn = 1 // 3.2.2
-    gameStatus.textContent = "Time to start! It's X's turn"
     winner = null // 3.2.3
+    waitingForTimeout = false
+    seconds = 0
+    clearInterval(tickInterval)
+    tickInterval = setInterval(tick, 1000)
+    resetDiv.classList.add("hidden")
     render () // 3.2.4
-    //console.log(boardArray)
+}
+
+function tick() {
+  seconds++  
 }
 
 function render() {
+  gameStatus.textContent = message
 	boardArray.forEach((square, idx) => {
-    //console.log(squaresArray[idx], square, idx)
     if (square === 1) {
       squaresArray[idx].textContent = 'X'
       squaresArray[idx].style.backgroundColor = 'red';
@@ -79,7 +92,6 @@ function render() {
 }
 
 function handleClick (event) {
-  console.log(event.target.id)
   const index = event.target.id.replace('sq', '') // 5.4
   if (boardArray[index] !== null) { // 5.2
     return
